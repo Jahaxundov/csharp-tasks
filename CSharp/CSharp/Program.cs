905bcd8f1d47878d02cff174d224b21de8881761
Jahaxundov <jahangirea@code.edu.az>
2023-07-13 01:06:00 +0400

    C#-Intro

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,77 @@
+﻿//1.Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+//int n = 22;
+
+//if (n % 21 == 0)
+//{
+//    Console.WriteLine("Bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+
+//2.n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+//int n = 12;
+//int m = 16;
+//int sum;
+
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//    sum = n + m;
+//    Console.WriteLine(sum);
+//}
+//else
+//{
+//    Console.WriteLine("N ve ya M cut deyil !");
+//}
+
+//3.Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+//int n = 11;
+//int m = 17;
+//int sum = 0;
+
+//for (int i = n; i < m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//4.Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+
+//int[] numbers = { 2, 5, 26, 11 };
+//int sum = 0;
+
+//foreach (var num in numbers)
+//{
+//    if (num % 2 == 1)
+//    {
+//        sum += num;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//5.Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+//int[] numbers = { 3, 6, 27, 16 ,24,13,};
+//int count=0;
+
+//foreach (var num in numbers)
+//{
+//    if (num % 2 == 0)
+//    {
+//         count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+
+
+
